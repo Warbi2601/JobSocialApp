b1890573c151d9cf163a0d4f32b444357b6ce392
Warbi2601 <joshwarbi@hotmail.com>
2021-02-17 15:51:53 +0000

    added form validation and seperate error messages to login

--- JobSocialApp/JobSocialApp/Views/LoginView.xaml.cs
+++ JobSocialApp/JobSocialApp/Views/LoginView.xaml.cs
@@ -16,23 +16,50 @@ namespace JobSocialApp.Views
         public LoginView()
         {
             InitializeComponent();
-
-    }
-
-    private async void SignInClicked(object sender, EventArgs e)
-    {
-        // need to add validation and exception catches // crashes under certain circumstances (password less than 6)
-        try
-        {
-            var email = loginEmail.Text;
-            var password = loginPassword.Text;
-            await DependencyService.Get<IFirebaseAuthenticator>().LoginWithEmailAndPassword(email, password);
-            Application.Current.MainPage = new HomeView();
-        } catch (Exception ex)
-        {
-            Console.WriteLine(ex);
         }
-    }
+
+        private async void SignInClicked(object sender, EventArgs e)
+        {
+            // need to add validation and exception catches // crashes under certain circumstances (password less than 6)
+            try
+            {
+                var email = loginEmail.Text; // valid email
+                var password = loginPassword.Text; // min 6 chars
+
+                bool validEmail = IsValidEmail(email);
+                bool validPass = password.Length >= 6;
+
+                if(!validEmail || !validPass)
+                {
+                    if (!validEmail) await DisplayAlert("Login Failed", "Email is not valid", "Ok");
+                    if (!validPass) await DisplayAlert("Login Failed", "Password does not meet requirements", "Ok");
+                }
+                else
+                {
+                    var a = await DependencyService.Get<IFirebaseAuthenticator>().LoginWithEmailAndPassword(email, password);
+                    Application.Current.MainPage = new HomeView();
+                }
+            }
+            catch (Exception ex)
+            {
+                if(ex.GetType().ToString() == "Firebase.Auth.FirebaseAuthInvalidUserException") 
+                    await DisplayAlert("Login Failed", "Email or Password incorrect", "Ok");
+                Console.WriteLine(ex);
+            }
+        }
+
+        bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
 
         private async void RegisterClicked(object sender, EventArgs e)
         {
