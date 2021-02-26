50b61dacb7f12f910a68436d907de5a120e78d60
dancutts90 <44802062+dancutts90@users.noreply.github.com>
2021-02-26 16:42:41 +0000

    translation manager
    Added translation manager class

---
+++ JobSocialApp/JobSocialApp/Models/TranslationManager.cs
@@ -0,0 +1,42 @@
+﻿using Java.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobSocialApp.Models
+{
+    class TranslationManager
+    {
+        private static readonly TranslationManager instance = new TranslationManager();
+        private static Dictionary<string, string> engMap = new Dictionary<string, string>();
+        private static Dictionary<string, string> polMap = new Dictionary<string, string>();
+
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static TranslationManager()
+        {
+            engMap.Add("title", "Launch your new career now!");
+            polMap.Add("title", "KURWA!");
+        }
+
+        private TranslationManager()
+        {
+
+        }
+
+        public static TranslationManager Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public string getTranslation(string key)
+        {
+            return engMap["title"]; 
+        }
+
+    }
+}
