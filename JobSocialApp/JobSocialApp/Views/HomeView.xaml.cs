e9e6a9f98677141ef6daf48d5d2b77852e78fd5e
Warbi2601 <joshwarbi@hotmail.com>
2021-03-05 13:03:36 +0000

    Notifications in with an example on the home screen

--- JobSocialApp/JobSocialApp/Views/HomeView.xaml.cs
+++ JobSocialApp/JobSocialApp/Views/HomeView.xaml.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms.Xaml;
 using JobSocialApp.ViewModels;
+using JobSocialApp.Models;
 
 namespace JobSocialApp.Views
 {
@@ -14,6 +15,10 @@ namespace JobSocialApp.Views
     public partial class HomeView : ContentPage
     {
         private HomeViewModel homeVM = null;
+
+        INotificationManager notificationManager;
+        int notificationNumber = 0;
+
         public HomeView()
         {
             InitializeComponent();
@@ -26,7 +31,46 @@ namespace JobSocialApp.Views
                 homeVM.setTitle();
             }
 
+            notificationManager = DependencyService.Get<INotificationManager>();
+            notificationManager.NotificationReceived += (sender, eventArgs) =>
+            {
+                var evtData = (NotificationEventArgs)eventArgs;
+                ShowNotification(evtData.Title, evtData.Message);
+            };
+
         }
+
+        void OnSendClick(object sender, EventArgs e)
+        {
+            //This one is an example of sending an instant notification
+            notificationNumber++;
+            string title = $"Local Notification #{notificationNumber}";
+            string message = $"You have now received {notificationNumber} notifications!";
+            notificationManager.SendNotification(title, message);
+        }
+
+        void OnScheduleClick(object sender, EventArgs e)
+        {
+            //This one is an example of sending a notification with a delay, uses intents so might be worth trying to get in?
+            notificationNumber++;
+            string title = $"Local Notification #{notificationNumber}";
+            string message = $"You have now received {notificationNumber} notifications!";
+            notificationManager.SendNotification(title, message, DateTime.Now.AddSeconds(10));
+        }
+
+        void ShowNotification(string title, string message)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                var msg = new Label()
+                {
+                    Text = $"Notification Received:\nTitle: {title}\nMessage: {message}"
+                };
+                stackLayout.Children.Add(msg);
+            });
+        }
+
+
 
     }
 }
